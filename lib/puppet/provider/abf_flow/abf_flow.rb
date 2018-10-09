d45d5271cdf7bcd35d67248849c6c497c66457a6
almogcohen <almog1232@gmail.com>
2018-10-09 10:22:46 -0700

    implement abf_flow provider

--- lib/puppet/provider/abf_flow/abf_flow.rb
+++ lib/puppet/provider/abf_flow/abf_flow.rb
@@ -3,27 +3,47 @@ require 'puppet/resource_api/simple_provider'
 # Implementation for the abf_flow type using the Resource API.
 class Puppet::Provider::AbfFlow::AbfFlow < Puppet::ResourceApi::SimpleProvider
   def get(_context)
-    [
-      {
-        name: 'foo',
-        ensure: 'present',
-      },
-      {
-        name: 'bar',
-        ensure: 'present',
-      },
-    ]
+    context.notice("Creating '#{name}' with #{should.inspect}")
+    validate_should(should) if defined? validate_should
+    app_revision_id = context.device.api.get_app_revision_id_by_name(should['application'])
+    context.device.api.create_application_flow(
+      app_revision_id,
+      name,
+      should['sources'],
+      should['destinations'],
+      should.fetch('services', []),
+      should.fetch('users', []),
+      should.fetch('applications', []),
+      should.fetch('comment', ''),
+      )
   end
 
   def create(context, name, should)
     context.notice("Creating '#{name}' with #{should.inspect}")
+    validate_should(should) if defined? validate_should
+    app_revision_id = context.device.api.get_app_revision_id_by_name(should['application'])
+    context.device.api.create_application_flow(
+      app_revision_id,
+      name,
+      should['sources'],
+      should['destinations'],
+      should.fetch('services', []),
+      should.fetch('users', []),
+      should.fetch('applications', []),
+      should.fetch('comment', ''),
+    )
   end
 
   def update(context, name, should)
-    context.notice("Updating '#{name}' with #{should.inspect}")
+    # Currently PUT is not implemented for flows so we simply delete and re-create
+    delete(context, name)
+    create(context, name, should)
   end
 
   def delete(context, name)
     context.notice("Deleting '#{name}'")
+    app_revision_id = context.device.api.get_app_revision_id_by_name(should['application'])
+    flow_id = context.device.api.get_application_flow_by_name(name)['flowID']
+    context.device.api.delete_flow_by_id(app_revision_id, flow_id)
   end
 end
